47bc86972b3a68d8d8638ffb2faeca088357f27e
dave <davebrown211@gmail.com>
2019-12-02 22:01:58 -0600

    initial commit

---
+++ new_contact.js
@@ -0,0 +1,6 @@
+function onSave() {
+  const jobTitleValue = Xrm.Page.getAttribute("jobtitle").getValue()
+  const firstNameAttribute = Xrm.Page.getAttribute("firstname")
+  const firstNameValue = firstNameAttribute.getValue()
+  firstNameAttribute.setValue(`${jobTitleValue} ${firstNameValue}`)
+}
